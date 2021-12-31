c361e3334659aae55c0cb913cfdce2ec6b8d4c77
Movsar Makhmutov <melkoto@mail.ru>
2022-01-01 01:51:12 +0300

    first

---
+++ cmd/web/routes_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"booking-go-app/internal/config"
+	"fmt"
+	"github.com/go-chi/chi"
+	"testing"
+)
+
+func TestRoutes(t *testing.T) {
+	var app config.AppConfig
+
+	mux := routes(&app)
+
+	switch v := mux.(type) {
+	case *chi.Mux:
+		// do nothing
+	default:
+		t.Error(fmt.Sprintf("type is not *chi.Mux, but is %T", v))
+	}
+}
